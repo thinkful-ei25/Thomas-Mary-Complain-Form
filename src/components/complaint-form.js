480512f097e46984e584e699f6987259b43af30c
mkcnly <mkcnly@gmail.com>
2018-11-14 11:47:29 -0800

    added all Inputs onto form

--- src/components/complaint-form.js
+++ src/components/complaint-form.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { reduxForm, Field } from 'redux-form';
-import { required, nonEmpty } from '../validators';
+import { required, nonEmpty, validLength, allNumbers } from '../validators';
 
 export class ComplaintForm extends React.Component {
     render() {
@@ -8,18 +8,31 @@ export class ComplaintForm extends React.Component {
             <div className="complaintForm">
                 <h2>Report a problem with your delivery</h2>
                 <form onSubmit={this.props.handleSubmit(values => this.onSubmit(values))}>
-                    <label htmlFor="trackingNumber">Tracking number</label>
-                    <Field component="input" name="trackingNumber"></Field>
-                    <label htmlFor="issue">What is your issue?</label>
-                    <Field component="select" name="issue">
+                    <Field
+                        component={Input}
+                        name="trackingNumber"
+                        label="Tracking Number"
+                        validate={[required, nonEmpty, validLength, allNumbers]}
+                        />
+                    <Field 
+                    component={Input}
+                    element="select" 
+                    name="issue"
+                    label="What is your issue?"
+                    validate={[required, nonEmpty]}>
                         <option value="not-delivered">My delivery has not arrived</option>
                         <option value="wrong-item">The wrong item was delivered</option>
                         <option value="missing-part">Part of my order was missing</option>
                         <option value="damaged">My order was damaged</option>
                         <option value="other">Other (give details below)</option>
                     </Field>
-                    <label htmlFor="details">Please provide more details (optional)</label>
-                    <Field component="textarea" name="details"></Field>
+                
+                    <Field 
+                    component={Input}
+                    element="textarea" 
+                    name="details"
+                    label="Please provide more details (optional)"
+                    />
                     <button type="submit">Submit</button>
                 </form>
             </div>
--- src/validators.js
+++ src/validators.js
@@ -1,3 +1,5 @@
+
 export const required = value => (value ? undefined : 'Required');
 export const nonEmpty = value => value.trim() !== '' ? undefined : 'Cannot be empty';
-export const email = value => /^\S+@\S+$/.test(value) ? undefined : 'Must be a valid email address';
+export const validLength = value => value.trim().length === 5 ? undefined : 'Must be 5 numbers in length';
+export const allNumbers = value => (value !== NaN ? undefined : 'Must be all numbers');
