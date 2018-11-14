071944eada68c6a07b4163fcdaeec5d19512d5a3
Thomas Chun <tchun2055@gmail.com>
2018-11-14 09:45:45 -0800

    added validators

--- src/components/complaint-form.js
+++ src/components/complaint-form.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { reduxForm, Field } from 'redux-form';
+import { required, nonEmpty } from '../validators';
 
-class ComplainForm extends React.Component {
+export class ComplaintForm extends React.Component {
     render() {
         return (
             <div className="complaintForm">
                 <h2>Report a problem with your delivery</h2>
-                <form>
+                <form onSubmit={this.props.handleSubmit(values => this.onSubmit(values))}>
                     <label htmlFor="trackingNumber">Tracking number</label>
                     <Field component="input" name="trackingNumber"></Field>
-                    <label htmlFor="issue">What is your issue></label>
+                    <label htmlFor="issue">What is your issue?</label>
                     <Field component="select" name="issue">
                         <option value="not-delivered">My delivery has not arrived</option>
                         <option value="wrong-item">The wrong item was delivered</option>
@@ -17,8 +18,15 @@ class ComplainForm extends React.Component {
                         <option value="damaged">My order was damaged</option>
                         <option value="other">Other (give details below)</option>
                     </Field>
+                    <label htmlFor="details">Please provide more details (optional)</label>
+                    <Field component="textarea" name="details"></Field>
+                    <button type="submit">Submit</button>
                 </form>
             </div>
         );
     }
 }
+
+export default reduxForm({
+    form: 'complaint'
+})(ComplaintForm);
